270577dbb9cbf396393cc5b4654883ebce2d167b
Aravind462 <aravindas2003@gmail.com>
2024-10-16 23:29:15 +0530

    v1

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  const [color,setColor] = useState("white");
+
+  const colors = ["Red","Blue","Green","Purple"]
+
+  return (
+    <>
+      <div style={{height:'100vh', width:'100%', backgroundColor:color}}>
+        <div className='text-center content'>
+          <h1 className='mb-3'>Background Color Change</h1>
+          <div>
+            {colors.map(item=>(
+              <button onClick={()=>setColor(item)} key={item} style={{backgroundColor:item}} className='btn mx-2 fw-bolder'>{item}</button>
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
